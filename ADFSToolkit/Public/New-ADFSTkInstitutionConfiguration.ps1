627030f046187eccaac3f6c91f0ffcd4adfe7f9c
Chris Phillips <cjphillips@gmail.com>
2020-11-10 00:09:34 -0500

    refactored global path  setups
    -[x] refactored path setups for new-adfstkconfiguration, new-adfstkinstitutionConfiguration, get-adfstkfederationdefaults

--- ADFSToolkit/Public/New-ADFSTkInstitutionConfiguration.ps1
+++ ADFSToolkit/Public/New-ADFSTkInstitutionConfiguration.ps1
@@ -12,14 +12,6 @@ function New-ADFSTkInstitutionConfiguration {
         $Global:ADFSTkPaths = Get-ADFSTKPaths
     }
    
-    #Create main dirs
-    ADFSTk-TestAndCreateDir -Path $Global:ADFSTkPaths.mainDir               -PathName "ADFSTk install directory" #C:\ADFSToolkit
-    ADFSTk-TestAndCreateDir -Path $Global:ADFSTkPaths.mainConfigDir         -PathName "Main configuration" #C:\ADFSToolkit\config
-    ADFSTk-TestAndCreateDir -Path $Global:ADFSTkPaths.mainBackupDir         -PathName "Main backup" #C:\ADFSToolkit\config\backup
-    ADFSTk-TestAndCreateDir -Path $Global:ADFSTkPaths.cacheDir              -PathName "Cache directory" #C:\ADFSToolkit\cache
-    ADFSTk-TestAndCreateDir -Path $Global:ADFSTkPaths.institutionDir        -PathName "Institution config directory" #C:\ADFSToolkit\config\institution
-    ADFSTk-TestAndCreateDir -Path $Global:ADFSTkPaths.institutionBackupDir  -PathName "Institution backup directory" #C:\ADFSToolkit\config\institution\backup
-    ADFSTk-TestAndCreateDir -Path $Global:ADFSTkPaths.federationDir         -PathName "Federation config directory" #C:\ADFSToolkit\config\federation   
     
     try {
         $mainConfiguration = Get-ADFSTkConfiguration
