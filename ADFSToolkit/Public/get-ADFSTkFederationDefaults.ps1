fa098ef9964bc4e7c56627e571ae4e0b1bf4d776
Chris Phillips <cjphillips@gmail.com>
2020-11-12 21:24:02 -0500

    language updates for feddefaults cmdlet
    -[x] updated to language strings

--- ADFSToolkit/Public/get-ADFSTkFederationDefaults.ps1
+++ ADFSToolkit/Public/get-ADFSTkFederationDefaults.ps1
@@ -36,17 +36,20 @@ process
 #
 #
 
-if  ($ClearCache )
+if  ($PSBoundParameters.ContainsKey('ClearCache') -and ($ClearCache -ne $null) )
 {
 
-    If  ( ( $PSBoundParameters.ContainsKey('InstallDefaults') -and ( $InstallDefaults -ne $null)  ) -or !( $PSBoundParameters.ContainsKey('URL') -and ($URL -eq  $null) )  )
+    If  ( ($PSBoundParameters.ContainsKey('InstallDefaults')) -or ($PSBoundParameters.ContainsKey('URL'))    )
     {
-        Write-Output ("ADFSToolkit: When used, ClearCache flag must be the only flag used. Nothing done, exiting ")
+        #("ADFSToolkit: When used, ClearCache flag must be the only flag used. Nothing done, exiting ")
+        Write-ADFSTkHost feddefaultsErrorFlags -Style Info
         return
 
     }else
     {
-        Write-Output ("ADFSToolkit: Removing cache file $federationConfigCacheFile")
+        #Write-Output ("ADFSToolkit: Removing cache file $federationConfigCacheFile")
+        Write-ADFSTkHost feddefaultsErrorFlagsElse -f $federationConfigCacheFile -Style Info
+
         remove-item  $federationConfigCacheFile
         return
 
@@ -58,11 +61,14 @@ if ( ( $PSBoundParameters.ContainsKey('URL') -and ($URL -eq  $null) ) -and !(Tes
    
     {
         
-            Write-Output ("ADFSToolkit: Federation default behaviour expected with no extra federation settings.")
+            #Write-Output ("ADFSToolkit: Federation default behaviour expected with no extra federation settings.")
+            Write-ADFSTkHost feddefaultsCaseNoURLNoCacheFile -Style info
+
         
             if ($InstallDefaults)
             {
-             Write-Output ("ADFSToolkit: InstallDefaults flag  found  but no defaults to apply, nothing changed, exiting.")
+             #Write-Output ("ADFSToolkit: InstallDefaults flag  found  but no defaults to apply, nothing changed, exiting.")
+             Write-ADFSTkHost feddefaultsCaseInstallDefNothing -Style Info
             }
 
 
@@ -71,7 +77,8 @@ if ( ( $PSBoundParameters.ContainsKey('URL') -and ($URL -eq  $null) ) -and !(Tes
 # Use case B: no url, config cache detected, tell user about it
 elseif ( (  $PSBoundParameters.ContainsKey('URL') -and ($URL -eq  $null) ) -and (Test-Path ( $federationConfigCacheFile )  ) )
      {
-        Write-Output ("ADFSToolkit: Federation defaults in the cache are:")
+        #Write-Output ("ADFSToolkit: Federation defaults in the cache are:")
+        Write-ADFSTkHost feddefaultsCaseNoURLConfigCacheYesHeader -Style info
 
         Get-ChildItem $federationConfigCacheFile -Force | Select-Object FullName, CreationTime, LastAccessTime, LastWriteTime, Mode, Length
 
@@ -84,28 +91,32 @@ elseif ( (  $PSBoundParameters.ContainsKey('URL') -and ($URL -eq  $null) ) -and 
 if ( $PSBoundParameters.ContainsKey('URL') -and !($URL -eq $null)  )
 {
 
-    Write-Output ("ADFSToolkit: Updating federation defaults on disk from: $URL")
+    #Write-Output ("ADFSToolkit: Updating federation defaults on disk from: $URL")
+    Write-ADFSTkHost feddefaultsCaseNoURLConfigCacheYesHeaderURLOK -f $URL -Style info
 
     # make backup
     If (Test-Path ( $federationConfigCacheFile ) )
     {
-        Write-Output ("ADFSToolkit: Backing up $fedConfigCacheFile to $fedConfigCacheFileBackup")
+        #Write-Output ("ADFSToolkit: Backing up $fedConfigCacheFile to $fedConfigCacheFileBackup")
+        Write-ADFSTkHost feddefaultsCaseNoURLConfigCacheYesHeaderBackingUp -f $fedConfigCacheFile,$fedConfigCacheFileBackup -Style Info
         Copy-item $federationConfigCacheFile -Destination $federationConfigCacheFileBackup
     }
 
     # Begin fetch file  
     $start_time = Get-Date
 
-    Write-Output ("ADFSToolkit: Fetching $URL to $federationConfigCacheFile")
+    #Write-Output ("ADFSToolkit: Fetching $URL to $federationConfigCacheFile")
+    Write-ADFSTkHost feddefaultsFetchBegin -f $URL, $federationConfigCacheFilev -Style info
     [System.Net.WebClient]::New().DownloadFile($URL, $federationConfigCacheFile)
 
-    Write-Output "ADFSToolkit: Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+    #Write-Output "ADFSToolkit: Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+    Write-ADFSTkHost feddefaultsTimeTaken -f $((Get-Date).Subtract($start_time).Seconds) -Style info
 
     Get-ChildItem $federationConfigCacheFile -Force | Select-Object FullName, CreationTime, LastAccessTime, LastWriteTime, Mode, Length
 
     #  interrogate the file we just fetched
-    Write-Output ("ADFSToolkit: contents of Federation Cache file: $federationConfigCacheFile :")
-
+    #Write-Output ("ADFSToolkit: contents of Federation Cache file: $federationConfigCacheFile :")
+      Write-ADFSTkHost feddefaultsContents  -f $federationConfigCacheFile -Style Info
     
 
     $zip = [io.compression.zipfile]::OpenRead($federationConfigCacheFile)
@@ -119,8 +130,9 @@ if ( $PSBoundParameters.ContainsKey('URL') -and !($URL -eq $null)  )
     if ( ( $PSBoundParameters.ContainsKey('InstallDefaults')  -and ($InstallDefaults -ne $null) )   -and (Test-Path ( $federationConfigCacheFile ) )  )
     {
 
-        Write-Output ("ADFSToolkit: Installing federation defaults from: $federationConfigCacheFile ...")
-    
+        #Write-Output ("ADFSToolkit: Installing federation defaults from: $federationConfigCacheFile ...")
+        Write-ADFSTkHost feddefaultsInstalling -f $federationConfigCacheFile -style info
+
         $zip = [io.compression.zipfile]::OpenRead($federationConfigCacheFile)
         $unzippedDirName=($zip.Entries[0]).FullName
         $unzippedFullNameDir=Join-Path (Join-path $Global:ADFSTkPaths.cacheDir $unzippedDirName) $ExtractionFilter
@@ -146,13 +158,14 @@ if ( $PSBoundParameters.ContainsKey('URL') -and !($URL -eq $null)  )
         }
 
 
-        Write-Output ("ADFSToolkit: Done. Next time a new aggregate is configured, defaults will be used. Existing configurations should remain unchanged")
+        #Write-Output ("ADFSToolkit: Done. Next time a new aggregate is configured, defaults will be used. Existing configurations should remain unchanged")
+        Write-ADFSTkHost     feddefaultsUnchanged -Style Info
     }
      else
     {
         Write-Output " "
-        Write-Output "ADFSToolkit: Federation defaults not installed into ADFSToolkit. Specify -InstallDefaults to apply them."
-
+        #Write-Output "ADFSToolkit: Federation defaults not installed into ADFSToolkit. Specify -InstallDefaults to apply them."
+        Write-ADFSTkHost     feddefaultsNotInstalled -Style Info
     }
 
 
@@ -160,7 +173,8 @@ if ( $PSBoundParameters.ContainsKey('URL') -and !($URL -eq $null)  )
 
     
 
-Write-Output  ("ADFSToolkit: Federation Defaults done.")
+    Write-ADFSTkHost feddefaultsAllDone -Style Info
+
 
 }
 
