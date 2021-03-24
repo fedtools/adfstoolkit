92a7832defcf6211f4644e1242e42f81be6c5d68
Chris Phillips <chris.phillips@canarie.ca>
2021-03-23 22:03:04 -0400

    Initial commit on MFA work
    Initial commit on separate package for ADFSToolkit  REFEDS MFA  support

---
+++ ADFSToolkitMFA/Private/ADFSTk-TestAndCreateDir.ps1
@@ -0,0 +1,32 @@
+﻿function ADFSTk-TestAndCreateDir
+{
+param (
+    # The path to test (and create if not exists)
+        [Parameter(Mandatory=$true,
+                   Position=0)]
+    $Path,
+    # The name of the directory, just for output
+        [Parameter(Mandatory=$false,
+                   Position=1)]
+    $PathName = 'The directory'
+)
+
+    if (Test-Path $Path)
+    {
+        #Write-ADFSTkVerboseLog (Get-ADFSTkLanguageText cPathExistsAtPath -f $PathName, $Path)
+    }
+    else
+    {
+        New-Item -ItemType Directory -Force -Path $Path | Out-Null
+        #Write-ADFSTkVerboseLog  (Get-ADFSTkLanguageText cPathNotExistCreatingHere -f $PathName, $Path)
+    }
+}
+
+<#
+.Synopsis
+   Checks if a directory exists and if not, creates it
+.DESCRIPTION
+   Checks if a directory exists and if not, creates it
+.EXAMPLE
+   ADFSTk-TestAndCreateDir -Path 'C:\ADFSTk' -PathName 'ADFSTk install directory'
+#>
