599b90151f1ae4bf04d927c75066ff335e8df0af
Chris Phillips <chris.phillips@canarie.ca>
2021-03-23 22:16:15 -0400

    Initial Commit of Source to Build adapter dll

---
+++ src/ADFSToolkitRefedsAdapters/ADFSToolkitRefedsAdapters/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ADFSToolkit Adapters")]
+[assembly: AssemblyDescription("Enables ADFS to handle Refeds MFA and SFA together with ADFSToolkit")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SUNET")]
+[assembly: AssemblyProduct("Refeds Authentication Providers")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("34229BED-BC14-4C06-B4B3-BB3CF3656997")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/ADFSToolkitRefedsAdapters/ADFSToolkitRefedsAdapters/UsernamePasswordException.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Microsoft.IdentityServer.Web.Authentication.External;
+
+namespace ADFSTk
+{
+    class UsernamePasswordException : ExternalAuthenticationException
+    {
+        protected UsernamePasswordException()
+        {  }
+
+        public UsernamePasswordException(string message, IAuthenticationContext context) : base(message, context)
+        {
+        }
+
+        public UsernamePasswordException(string message, Exception ex, IAuthenticationContext context) : base(message, ex, context)
+        {
+        }
+    }
+
+    class UsernamePasswordValidationException : UsernamePasswordException
+    {
+        private UsernamePasswordValidationException()
+        { }
+
+        public UsernamePasswordValidationException(string message, IAuthenticationContext context) : base(message, context)
+        {
+        }
+        public UsernamePasswordValidationException(string message, Exception ex, IAuthenticationContext context) : base(message, ex, context)
+        {
+        }
+
+    }
+}
