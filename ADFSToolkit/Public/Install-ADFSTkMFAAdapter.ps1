cbec5503ce4081bec4fdff56f5ac10604bc4c53f
Johan Peterson <johan.peterson@liu.se>
2021-04-30 13:44:48 +0200

    Updated with language support

--- ADFSToolkit/Public/Install-ADFSTkMFAAdapter.ps1
+++ ADFSToolkit/Public/Install-ADFSTkMFAAdapter.ps1
@@ -16,12 +16,12 @@ function Install-ADFSTkMFAAdapter {
     $restart = $true
     
     if (!(Get-AdfsGlobalAuthenticationPolicy).AllowAdditionalAuthenticationAsPrimary) {
-        if (Get-ADFSTkAnswer "You need to enable the Additional Authentication As Primary property on AdfsGlobalAuthenticationPolicy. Do you want ADFSToolkitMFA to do that now?" -DefaultYes) {
+        if (Get-ADFSTkAnswer (Get-ADFSTkLanguageText mfaEnableAdditionalAuthenticationAsPrimaryQuestion) -DefaultYes) {
             Set-AdfsGlobalAuthenticationPolicy -AllowAdditionalAuthenticationAsPrimary $true -Force | Out-Null
         }
         else {
             $restart = $false
-            Write-ADFSTkLog "You have to enable the Additional Authentication as Primary property on AdfsGlobalAuthenticationPolicy to continue!" -MajorFault
+            Write-ADFSTkLog (Get-ADFSTkLanguageText mfaEnableAdditionalAuthenticationAsPrimaryNeeded) -MajorFault
         }
     }
 
@@ -50,45 +50,44 @@ function Install-ADFSTkMFAAdapter {
     #endregion
 
     #region GAC the Dll's
-    Write-ADFSTkVerboseLog "Getting path for dll file..."
-    $modulePath = Get-Module -ListAvailable ADFSToolkitMFA | Sort-Object Version -Descending | Select -First 1 | Select -ExpandProperty ModuleBase
-    $binPath = Join-Path $modulePath Bin
+    Write-ADFSTkVerboseLog (Get-ADFSTkLanguageText mfaGettingPathForDll)
+    $binPath = Join-Path $global:ADFSTkPaths.modulePath Bin
     $dllFile = Join-Path $binPath 'ADFSToolkitAdapters.dll'
-    Write-ADFSTkVerboseLog "Found dll file with the following path: $dllFile"
+    Write-ADFSTkVerboseLog (Get-ADFSTkLanguageText mfaDllFound -f $dllFile)
 
-    Write-ADFSTkVerboseLog "Loading System.EnterpriseSerevices Assebbly..."
+    Write-ADFSTkVerboseLog (Get-ADFSTkLanguageText mfaLoadingAssembly -f "System.EnterpriseSerevices") 
     [System.Reflection.Assembly]::Load("System.EnterpriseServices, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a") | Out-Null
-    Write-ADFSTkVerboseLog "Done!"
+    Write-ADFSTkVerboseLog (Get-ADFSTkLanguageText cDone)
         
-    Write-ADFSTkVerboseLog "Executing GacInstall on dll file..."
+    Write-ADFSTkVerboseLog (Get-ADFSTkLanguageText mfaExecutingGacInstall)
     $publish = New-Object System.EnterpriseServices.Internal.Publish
     $publish.GacInstall($dllFile)
-    Write-ADFSTkVerboseLog "Done!"
+    Write-ADFSTkVerboseLog (Get-ADFSTkLanguageText cDone)
     
-    Write-ADFSTkVerboseLog "Loading dll file Assembly..."
+    Write-ADFSTkVerboseLog (Get-ADFSTkLanguageText mfaLoadingAssembly -f "dll file")
     $fn = ([System.Reflection.Assembly]::LoadFile($dllFile)).FullName
-    Write-ADFSTkVerboseLog "Done!"
+    Write-ADFSTkVerboseLog (Get-ADFSTkLanguageText cDone)
     #endregion
     
     if ($PSCmdlet.ParameterSetName -eq 'RefedsMFA') {
-        Write-ADFSTkVerboseLog "Entering RefedsMFA path..."
+        Write-ADFSTkVerboseLog (Get-ADFSTkLanguageText mfaEnteringPath -f "RefedsMFA")
 
         $nameMFA = "RefedsMFAUsernamePasswordAdapter"
         if ($authProviders.Name.contains($nameMFA)) {
             $restart = $false
-            Write-ADFSTkLog "RefedsMFA Adapter already installed! Please uninstall first to continue (Uninstall-ADFSTkMFAAdapter -RefedsMFA)." -EntryType Warning
+            Write-ADFSTkLog (Get-ADFSTkLanguageText mfaAdapterAlreadyInstalled -f "RefedsMFA") -EntryType Warning
         }
         else {
-            Write-ADFSTkVerboseLog "Retrieving Type Name for RefedsMFAUsernamePasswordAdapter..."
+            Write-ADFSTkVerboseLog (Get-ADFSTkLanguageText mfaRetrievingTypeName)
             $typeNameMFA = "ADFSTk.RefedsMFAUsernamePasswordAdapter, " + $fn.ToString() + ", processorArchitecture=MSIL"
             Write-ADFSTkVerboseLog "TypeName: $typeNameMFA"
         
-            Write-ADFSTkVerboseLog "Registerings Authentication Provider..."
+            Write-ADFSTkVerboseLog (Get-ADFSTkLanguageText mfaRegisteringAuthProvider)
             Register-AdfsAuthenticationProvider -TypeName $typeNameMFA -Name $nameMFA  | Out-Null
-            Write-ADFSTkVerboseLog "Done!"
+            Write-ADFSTkVerboseLog (Get-ADFSTkLanguageText cDone)
         
-            Write-ADFSTkHost "Installation done!" -Style Done
-            if (Get-ADFSTkAnswer "Do you want to register 'Forms Authentication (RefedsMFA)' as a primary Authentication Provider?" -DefaultYes) {
+            Write-ADFSTkHost cInstallationDone -Style Done
+            if (Get-ADFSTkAnswer (Get-ADFSTkLanguageText mfaRegisterAuthenticationProviderQuestion -f "Forms Authentication (RefedsMFA)") -DefaultYes) {
                 ##Register 
                 $authPolicy = Get-AdfsGlobalAuthenticationPolicy
                 Set-AdfsGlobalAuthenticationPolicy -PrimaryExtranetAuthenticationProvider ($authPolicy.PrimaryExtranetAuthenticationProvider + $nameMFA) `
@@ -100,24 +99,24 @@ function Install-ADFSTkMFAAdapter {
         }
     }
     elseif ($PSCmdlet.ParameterSetName -eq 'RefedsSFA') {
-        Write-ADFSTkVerboseLog "Entering RefedsSFA path..."
+        Write-ADFSTkVerboseLog (Get-ADFSTkLanguageText mfaEnteringPath -f "RefedsSFA")
         $nameSFA = "RefedsSFAUsernamePasswordAdapter"
 
         if ($authProviders.Name.contains($nameSFA)) {
             $restart = $false
-            Write-ADFSTkLog "RefedsSFA Adapter already installed! Please uninstall first to continue (Uninstall-ADFSTkMFAAdapter -RefedsSFA)." -EntryType Warning
+            Write-ADFSTkLog (Get-ADFSTkLanguageText mfaAdapterAlreadyInstalled -f "RefedsSFA") -EntryType Warning
         }
         else {
-            Write-ADFSTkVerboseLog "Retrieving Type Name for RefedsMFAUsernamePasswordAdapter..."
+            Write-ADFSTkVerboseLog (Get-ADFSTkLanguageText mfaRetrievingTypeName)
             $typeNameSFA = "ADFSTk.RefedsSFAUsernamePasswordAdapter, " + $fn.ToString() + ", processorArchitecture=MSIL"
             Write-ADFSTkVerboseLog "TypeName: $typeNameMFA"
 
-            Write-ADFSTkVerboseLog "Registerings Authentication Provider..."
+            Write-ADFSTkVerboseLog (Get-ADFSTkLanguageText mfaRegisteringAuthProvider)
             Register-AdfsAuthenticationProvider -TypeName $typeNameSFA -Name $nameSFA | Out-Null
-            Write-ADFSTkVerboseLog "Done!"
+            Write-ADFSTkVerboseLog (Get-ADFSTkLanguageText cDone)
         
-            Write-ADFSTkHost "Installation done!" -Style Done
-            if (Get-ADFSTkAnswer "Do you want to register 'Forms Authentication (RefedsSFA)' as a primary Authentication Provider?" -DefaultYes) {
+            Write-ADFSTkHost cInstallationDone -Style Done
+            if (Get-ADFSTkAnswer (Get-ADFSTkLanguageText mfaRegisterAuthenticationProviderQuestion -f "Forms Authentication (RefedsSFA)") -DefaultYes) {
                 ##Register 
                 $authPolicy = Get-AdfsGlobalAuthenticationPolicy
                 Set-AdfsGlobalAuthenticationPolicy -PrimaryExtranetAuthenticationProvider ($authPolicy.PrimaryExtranetAuthenticationProvider + $nameSFA) `
@@ -129,7 +128,7 @@ function Install-ADFSTkMFAAdapter {
         }
     }
 
-    if ($restart -and (Get-ADFSTkAnswer "The ADFS Service needs to be restarted. Do you want to do that now?" -DefaultYes)) {
+    if ($restart -and (Get-ADFSTkAnswer (Get-ADFSTkLanguageText cRestartADFSServiceQuestion) -DefaultYes)) {
         net stop adfssrv
         net start adfssrv
     }
