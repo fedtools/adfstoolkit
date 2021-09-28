41c920bd58bfb4023108af1e31acc4d3b040b7e3
Johan Peterson <johan.peterson@liu.se>
2021-09-28 15:18:42 +0200

    Revert transient calculation to previous way

--- ADFSToolkit/Private/Import-ADFSTkAllTransformRules.ps1
+++ ADFSToolkit/Private/Import-ADFSTkAllTransformRules.ps1
@@ -113,7 +113,7 @@ $TransformRules."transient-id" = [PSCustomObject]@{
             param = c1.Value, 
             param = c1.OriginalIssuer, 
             param = "", 
-            param = regexreplace(c2.Value, "(?<start>^.{1,20}).+$", "${start}"));
+            param = c2.Value);
             
     @RuleName = "issue transient-id"
     c:[Type == "urn:adfstk:transientid"]
