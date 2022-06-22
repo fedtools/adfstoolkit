2387e996d5fb5d8124c455e019cc2ca44e242731
Johan Peterson <johan.peterson@liu.se>
2022-06-22 15:35:49 +0200

    Better Store rule

--- ADFSToolkit/Private/Get-ADFSTkStoreRule.ps1
+++ ADFSToolkit/Private/Get-ADFSTkStoreRule.ps1
@@ -8,8 +8,20 @@
     $FirstRule = ""
 
     # First check if ADFSTkStore is installed and transform attributes if needed
-    $ADFSTkStoreInstalled = [string]::IsNullOrEmpty((Get-AdfsAttributeStore -Name ADFSTkStore))
-    if ($ADFSTkStoreInstalled) {
+    $ADFSTkStore = Get-AdfsAttributeStore -Name ADFSTkStore
+    
+    if ([string]::IsNullOrEmpty($ADFSTkStore)) {
+        #ADFSTkStore is not installed
+        $fixedAttributes = $AttributesFromStore.Values
+        if (![string]::IsNullOrEmpty($AttributesFromStore.Values.transformvalue.adfstkstorefunction)) {
+            Write-ADFSTkLog @"
+ADFSTk Store is not intalled but attributes in the Institution configuration file is configured for it. 
+Please run Install-ADFSTkStore on all ADFS servers or remove the transformvalue attribute adfstkstorefunction.
+"@
+        }
+    }
+    else {
+        #ADFSTkStore is installed
         $fixedAttributes = foreach ($attrib in $AttributesFromStore.Values) {
             if (![string]::IsNullOrEmpty($attrib.transformvalue) -and $attrib.transformvalue.HasAttribute('adfstkstorefunction')) {
                 $newAttrib = $attrib.Clone()
@@ -20,16 +32,7 @@
                 $attrib
             }
         }
-    }
-    else {
-        $fixedAttributes = $AttributesFromStore.Values
-        if (![string]::IsNullOrEmpty($AttributesFromStore.Values.transformvalue.adfstkstorefunction))
-        {
-            Write-ADFSTkLog @"
-ADFSTk Store is not intalled but attributes in the Institution configuration file is configured for it. 
-Please run Install-ADFSTkStore on all ADFS servers or remove the transformvalue attribute adfstkstorefunction.
-"@
-        }
+        
     }
 
     foreach ($store in ($Stores | Sort order)) {
@@ -103,20 +106,39 @@ Please run Install-ADFSTkStore on all ADFS servers or remove the transformvalue 
         #endregion
 
         #region ADFSTkStore
-        if ($store.storetype -eq "Custom Store") {
-            $currentStoreAttributes = $fixedAttributes | ? store -eq $store.name
-            if ($currentStoreAttributes -ne $null) {
-                $FirstRule += @"
+        if (![string]::IsNullOrEmpty($ADFSTkStore)) {
+            
+            foreach ($attrib in $AttributesFromStore.Values) {
+                if (![string]::IsNullOrEmpty($attrib.transformvalue) -and $attrib.transformvalue.HasAttribute('adfstkstorefunction')) {
+                    $FirstRule += @"
 
-                @RuleName = "Retrieve Attributes from Custom Store"
-                c:[Type == "$($store.type)", Issuer == "$($store.issuer)"]
-                => add(store = "$($store.name)", 
-                types = ("$($currentStoreAttributes.type -join '","')"), 
-                query = ";$($currentStoreAttributes.name -join ',');{0}", param = "[ReplaceWithSPNameQualifier]", param = c.Value);
-
+                @RuleName = "Transform $($attrib.name) through Custom Store"
+                c:[Type == "$($attrib.type):beforetransform"]
+                => add(store = "ADFSTkStore", 
+                types = ("$($attrib.type)"), 
+                query = ";$($attrib.transformvalue.adfstkstorefunction);{0}", param = "[ReplaceWithSPNameQualifier]", param = "$($ADFSTkStore.Configuration.IDPSALT)", param = c.Value);
+                
 "@
+# c:[Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/displayname:beforetransform "]
+# => add(store = "ADFSTKStore", types = ("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/displayname "), query = ";toupper;{0}", param = "entity", param = "salt", param = c.Value);
+                }
             }
         }
+
+        #         if ($store.storetype -eq "Custom Store") {
+        #             $currentStoreAttributes = $fixedAttributes | ? store -eq $store.name
+        #             if ($currentStoreAttributes -ne $null) {
+        #                 $FirstRule += @"
+
+        #                 @RuleName = "Retrieve Attributes from Custom Store"
+        #                 c:[Type == "$($store.type)", Issuer == "$($store.issuer)"]
+        #                 => add(store = "$($store.name)", 
+        #                 types = ("$($currentStoreAttributes.type -join '","')"), 
+        #                 query = ";$($currentStoreAttributes.name -join ',');{0}", param = "[ReplaceWithSPNameQualifier]", param = c.Value);
+
+        # "@
+        #             }
+        #         }
         #endregion
     }
 
